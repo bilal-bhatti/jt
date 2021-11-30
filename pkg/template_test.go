9ca58b283a9da051e54c5e17aa54f2c6476d2810
Bilal Bhatti <bbhatti@gmail.com>
2021-11-29 19:55:59 -0600

    fix test

--- pkg/template_test.go
+++ pkg/template_test.go
@@ -12,10 +12,14 @@ func TestTemplatization(t *testing.T) {
 	var c interface{}
 
 	yf, err := ioutil.ReadFile("template_test.json")
-	t.Error(err)
+	if err != nil {
+		t.Error(err)
+	}
 
 	err = json.Unmarshal(yf, &c)
-	t.Error(err)
+	if err != nil {
+		t.Error(err)
+	}
 
 	for _, data := range c.(map[string]interface{})["data"].([]interface{}) {
 		input := data.(map[string]interface{})["input"]
@@ -23,6 +27,6 @@ func TestTemplatization(t *testing.T) {
 
 		Templatize(input)
 
-		assert.Equal(t, expected, input)
+		assert.Equal(t, expected, input, "equal")
 	}
 }
